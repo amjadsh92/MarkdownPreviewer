0452cced4527dea1d56eb19d0b2d3ebcf60d5d71
amjadsh92 <amjadsharafeddien@gmail.com>
2025-03-07 15:56:32 +0200

    Expand and minimize the Editor box

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import './App.css'
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faMaximize} from "@fortawesome/free-solid-svg-icons";
+import {faDownLeftAndUpRightToCenter} from "@fortawesome/free-solid-svg-icons";
 
 //test
 function App() {
@@ -22,9 +24,23 @@ function App() {
 
 function Editor(){
 
+  const [isExpanded, setIsExpanded] = useState(false)
+
+  function expand(){
+    setIsExpanded(true)
+
+  }
+
+  function minimize(){
+    setIsExpanded(false)
+
+  }
+
+
+                
   return (
-    <div id="editor" className="editor mx-auto border border-dark">
-      <div id="header" className="d-flex header text-black fs-6 justify-content-between align-items-start"><span className="editor-title align-text-top">Editor</span> <FontAwesomeIcon  icon={faMaximize} className="maximize" /></div>
+    <div id="editor" className ={`${isExpanded ? "editor-expanded" : "editor"} mx-auto border border-dark`}>
+      <div id="header" className="d-flex header text-black fs-6 justify-content-between align-items-start"><span className="editor-title align-text-top">Editor</span> {isExpanded ? <FontAwesomeIcon className="minimize" icon={faDownLeftAndUpRightToCenter} onClick={minimize} /> : <FontAwesomeIcon  icon={faMaximize} className="maximize" onClick={expand} /> }</div>
       <textarea className = "text-area border-0 w-100 form-control border-0 shadow-none" > </textarea>
     </div>
   )
